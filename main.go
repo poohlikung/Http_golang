c89c0befaf50f3eaece11bdf8548128fa73d7eb6
PoohSorawit <118596123+poohlikung@users.noreply.github.com>
2026-05-17 01:36:23 +0700

    first fiber
    path hello world

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world isus")
+	})
+
+	app.Listen(":8080")
+}
