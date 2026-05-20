69e9ad0ac336cad9c32b7357d7a7b2b814d06151
PoohSorawit <118596123+poohlikung@users.noreply.github.com>
2026-05-21 02:44:39 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,24 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// ประกาศ struct ในการกำหนดแม่แบบ
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world isus")
+	books = append(books, Book{ID: 1, Title: "Poohlikung", Author: "Pooh"})
+
+	books = append(books, Book{ID: 2, Title: "Paleerat", Author: "Nampung"})
+
+	app.Get("/books", func(c *fiber.Ctx) error {
+		return c.JSON(books)
 	})
 
 	app.Listen(":8080")
