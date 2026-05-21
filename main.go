ac36e509163a0f394e4afec43f037883cf245fe8
PoohSorawit <118596123+poohlikung@users.noreply.github.com>
2026-05-21 14:35:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -24,8 +24,11 @@ func main() {
 
 	app.Get("/books", getBooks)
 
+	app.Post("/books", createBooks)
 	// one pice
 	app.Get("/book/:id", getBook)
+	// Put
+	app.Put("/books/:id", updateBook)
 
 	app.Listen(":8080")
 }
@@ -43,6 +46,35 @@ func getBook(c *fiber.Ctx) error {
 	for _, book := range books {
 		if book.ID == bookid {
 			return c.JSON(book)
+		}
+	}
+	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func createBooks(c *fiber.Ctx) error {
+	book := new(Book)
+
+	if err := c.BodyParser(book); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	books = append(books, *book)
+	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	bookId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	bookUpdate := new(Book)
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	for i, book := range books {
+		if book.ID == bookId {
+			books[i].Title = bookUpdate.Title
+			books[i].Author = bookUpdate.Author
+			return c.JSON(books[i])
 		}
 	}
 	return c.SendStatus(fiber.StatusNotFound)
