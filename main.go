1802012f7d950fbe460bc0d3e6686adf2d08d66f
PoohSorawit <118596123+poohlikung@users.noreply.github.com>
2026-05-27 22:49:20 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"time"
-  "github.com/gofiber/jwt/v2"
-  "github.com/golang-jwt/jwt/v4"
-	"github.com/gofiber/fiber/v2"
 	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 // ประกาศ struct ในการกำหนดแม่แบบ
@@ -19,8 +19,16 @@ type Book struct {
 var books []Book
 
 func checkMiddleware(c *fiber.Ctx) error {
-	start := time.Now()
-	fmt.Printf("URL = %s,Method = %s,Time = %s\n", c.OriginalURL(), c.Method(), start)
+	// start := time.Now()
+	// fmt.Printf("URL = %s,Method = %s,Time = %s\n", c.OriginalURL(), c.Method(), start)
+
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+
+	if claims["role"] != "admin" {
+		return fiber.ErrUnauthorized
+	}
+
 	return c.Next()
 }
 
@@ -32,12 +40,16 @@ func main() {
 	books = append(books, Book{ID: 2, Title: "Paleerat", Author: "Nampung"})
 
 	app.Post("/login", loginUser)
-	  // JWT Middleware
-  app.Use(jwtware.New(jwtware.Config{
-    SigningKey: []byte(os.Getenv("JWT_SECRET")),
-  }))
+
 	// when login sucess middle are show
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	}))
+
 	app.Use(checkMiddleware)
+
 	app.Get("/books", getBooks)
 
 	app.Post("/books", createBooks)
@@ -86,7 +98,24 @@ func loginUser(c *fiber.Ctx) error {
 	if user.Email != memberUser.Email || user.Password != memberUser.Password {
 		return fiber.ErrUnauthorized
 	}
+
+	// Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["email"] = user.Email
+	claims["admin"] = "admin"
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token
+	t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
 	return c.JSON(fiber.Map{
-		"message": "login sucess",
+		"message": "Login success",
+		"token":   t,
 	})
 }
